cdaf512331871f69d9105743afe3068396acd5cd
Timotax <timotax100@gmail.com>
2022-07-01 18:00:58 -0700

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -30,8 +30,10 @@ int main(void)
 						putchar(' ');
 						putchar(e);
 						putchar(i);
-						if (h, e, i == 57 && g == 56)
+						if (h == 57 && g == 56 && e == 57 && i == 57)
+						{
 							break;
+						}
 						putchar(',');
 						putchar(' ');
 					}
