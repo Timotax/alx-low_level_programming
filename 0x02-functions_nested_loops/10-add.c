30d44e55070a548d875c88100ab88923f417edd8
Timotax <timotax100@gmail.com>
2022-07-05 13:52:16 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Addition of 2 int number
+ *
+ * @a: number one
+ * @b: number two
+ *
+ * Return: Add of number one and number two
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
