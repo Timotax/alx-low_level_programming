15305f102d3b8708662e186226f7e09367cb1698
Timotax <timotax100@gmail.com>
2022-07-07 08:54:03 -0700

    Print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the number
+ * from 0 to 9
+ *
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 58; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int x);
 int mul(int x, int y);
+void print_numbers(void);
 
 #endif
