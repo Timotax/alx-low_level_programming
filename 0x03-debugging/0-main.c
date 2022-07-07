871b653b0827259b90368457cb78e353f71a1474
Timotax <timotax100@gmail.com>
2022-07-07 06:33:19 -0700

    First main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,13 @@
 
 /**
  * main - test function that prints if integer is positive or negative
- *
  * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98;
 	positive_or_negative(1);
 
 	return (0);
