0bd1a90b021420f5c82d7e5ed15feb721fe25b34
Timotax <94500220+Timotax@users.noreply.github.com>
2022-07-15 06:47:43 -0700

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,7 +6,7 @@
  * @n2: number two.
  * @r: buffer that the function will use to store the result.
  * @size_r: buffer size:
- * Return: the pointer to dest.
+ * Return: the pointer to dest
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
