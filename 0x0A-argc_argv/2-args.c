19185feb75c5bb94181b6b1f028c514b27e54caf
Timotax <timotax100@gmail.com>
2022-07-26 15:01:02 -0700

    Charge throuh all task

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all arg it receives
  * @argc: number of command line arguments
- * #argv: array that contains the program aommand line arguments
+ * @argv: array that contains the program aommand line arguments
  * Return: 0 (success)
  */
 int main(int argc, int *argv[])
